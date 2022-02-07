093ecc18b89d92d1fb6009dbba4d7fa5a4f8be85
Azecko <sami.panseri@epfl.ch>
2022-02-07 12:03:59 +0100

    [feat] echo is approximately correct

--- update-contributors.sh
+++ update-contributors.sh
@@ -2,4 +2,4 @@ data=$(curl -s https://api.github.com/repos/epfl-dojo/contributeurs-trombinoscop
 
 echo "$data"
 # sed  's/unix/linux/' README.md
-sed -e "/BEGIN/,/END/c\BEGIN\n$(echo -e $data)\nEND" README.md
+sed -e "/BEGIN/,/END/c\BEGIN\n$(echo $data)\nEND" README.md
