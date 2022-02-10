d3b1b1e0ce05dcb4833bd0c98569104ce0194e4b
Nicolas Borboën <ponsfrilus@gmail.com>
2022-02-10 09:01:54 +0100

    [refactor] minimal viable product

--- update-contributors.sh
+++ update-contributors.sh
@@ -1,5 +1,22 @@
-data=$(curl -s https://api.github.com/repos/epfl-dojo/contributeurs-trombinoscope/contributors | jq -r '.[] | "  - [@\(.login)](\(.html_url))"')
+#!/usr/bin/env bash
+# set -e -x
 
-echo "$data"
-# sed  's/unix/linux/' README.md
-sed -e "/BEGIN/,/END/c\BEGIN\n$(echo -e $data)\nEND" README.md
+LEAD='^<!-- start_contributors .*-->$'
+TAIL='^<!-- end_contributors .*-->$'
+
+# TODO: get these from the current repo
+USER=epfl-dojo
+REPO=contributeurs-trombinoscope
+
+# Fetch the contributors list
+contrib_list () {
+  CONTRIBUTORS_LIST=$(curl -s https://api.github.com/repos/${USER}/${REPO}/contributors | jq -r '.[] | "  * [@\(.login)](\(.html_url))"'); \
+  echo "${CONTRIBUTORS_LIST}" > tmp_data
+  cat tmp_data
+}
+
+contrib_list
+sed -i -e "/$LEAD/,/$TAIL/{ /$LEAD/{p; r tmp_data
+}; /$TAIL/p; d }" README.md
+
+cat README.md
